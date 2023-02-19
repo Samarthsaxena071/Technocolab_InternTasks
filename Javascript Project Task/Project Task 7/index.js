dc7ed0adf451a1bb4b1c485b4f859d402fab3af7
Samarth Saxena <86877721+Samarthsaxena071@users.noreply.github.com>
2023-02-19 22:23:55 +0530

    Update index.js

--- Javascript Project Task/Project Task 7/index.js
+++ Javascript Project Task/Project Task 7/index.js
@@ -9,7 +9,7 @@ lateryears*=4;
 
 var myageindogyears = lateryears + earlyage;
 
-var name = "Apoorva";
+var name = "Samarth";
 var myName = name.toLowerCase();
 
 console.log("My name is "+ myName +" I am " + myage +  " years old in human years which is " + myageindogyears +" years old in dog years.");
